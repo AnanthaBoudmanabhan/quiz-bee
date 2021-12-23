0ced77cc9dd739d407055ab62e7c9b4cda1ffa11
Anantha Boudmanabhan <ananth1995.july22@gmail.com>
2021-12-23 14:49:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import QuestionBox from './components/QuestionBox';
+import Result from './components/Result';
+import questionService from './quizService/index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [questionBank, setQuestionBank] = useState([]);
+    const [score, setScore] = useState(0);
+    const [response, setResponse] = useState(0);
+
+    useEffect(() => {
+        getQuestionBank();
+    }, [])
+
+    const getQuestionBank = () => {
+        questionService().then(questions => setQuestionBank(questions));
+    }
+
+    const computeAnswer = (answer, correct) => {
+        if (answer === correct)
+            setScore(prevState => prevState + 1);
+        setResponse(prevState => prevState + 1);
+    }
+
+    const playAgain = () => {
+        getQuestionBank();
+        setScore(0);
+        setResponse(0);
+    }
+
+    return (
+        <div className="container">
+            <div className="title">QuizBee</div>
+            {response < 5 &&
+                questionBank.length > 0 && questionBank.map(({ questionId, question, answers, correct }) => (
+                    <QuestionBox
+                        question={question}
+                        options={answers}
+                        key={questionId}
+                        selected={answer => computeAnswer(answer, correct)}
+                    />
+                ))
+            }
+            {response === 5 && <Result score={score} playAgain={playAgain} />}
+        </div>
+    )
 }
 
 export default App;
